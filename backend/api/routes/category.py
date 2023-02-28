9cf51a26fcfa6794203793d477245cebb7864f2a
YaoEnn <hongdieuan.712000@gmail.com>
2023-02-28 13:47:12 +0700

    Feb 28th: 1. Finishing category.py 2. All functions were tested via Postman

--- backend/api/routes/category.py
+++ backend/api/routes/category.py
@@ -1,26 +1,135 @@
 from app import app
+# import db
+from db import Category, db
+from flask import request, jsonify
+from . import login_required
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy import func
 
 
 @app.route('/api/category/add', methods=['POST'])
+@login_required(role_allow=['manager', 'administrator'])
 def add_category():
-    return ""
+    #check if name is available?
+    data = request.json
+    if data and 'name' in data:
+        new_name = data['name'].strip()
+    else:
+        return jsonify({
+            'status':'FAIL',
+            'msg':'Missing Name'
+        })
+    # Add to database 
+    try:
+        new_category = Category(name = new_name)
+        db.session.add(new_category)
+        db.session.commit()
+        return jsonify({
+            'status':'OK',
+            'msg':'Add Successfully'
+        })
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({
+            'status':'FAILED',
+            'msg':'Failed to add specified category'
+        })
 
 @app.route('/api/category/delete/<category_id>', methods=['DELETE'])
-
+@login_required(role_allow=['manager', 'administrator'])
 def delete_category(category_id):
-    return ""
+    #check if category_id exist?
+    category = Category.query.get(category_id)
+    if category:
+        #delete category
+        try:
+            db.session.delete(category)
+            db.session.commit()
+            return jsonify({
+                'status':'OK',
+                'msg':'Category Deleted'
+            })
+        except:
+            db.session.rollback()
+            return jsonify({
+                'status':'FAILED',
+                'msg':'Could Not Delete Category'
+            })
+    else:
+        return jsonify({
+            'status':'FAILED',
+            'msg':'Category not found'
+        })
 
 
 @app.route('/api/category/list', methods=['POST'])
+@login_required(role_allow=['manager', 'administrator'])
 def list_all_category():
-    return ""
+    #get all category
+    categories = Category.query.all()
+    result = []
+    # Add categories to list
+    for category in categories:
+        category_data = {'id': category.id, 'name': category.name, 'created_on': category.created_on}
+        result.append(category_data)
+    return jsonify({
+        'status':'OK',
+        'msg': result
+    })
 
 
 @app.route('/api/category/get/<category_id>', methods=['POST'])
+@login_required(role_allow=['manager', 'administrator'])
 def get_category_info(category_id):
-    return ""
+    #check if category existed?
+    category = Category.query.get(category_id)
+    result = []
+    if category:
+        # Add category to list
+        result.append({'id': category.id, 'name': category.name, 'created_on': category.created_on})
+        return jsonify({
+            'status':'OK',
+            'msg':result
+        })
+    else:
+        return jsonify({
+            'status':"FAILED",
+            'msg':'No Category Found'
+        })
 
 
 @app.route('/api/category/update/<category_id>', methods=['POST'])
+@login_required(role_allow=['manager', 'administrator'])
 def update_category_info(category_id):
-    return ""
+    #check if category existed?
+    category = Category.query.get(category_id)
+    data = request.json
+    if category:
+        #check data and update category_name
+        if data and ("name" in data):
+            new_name = data['name'].strip()
+        else:
+            return jsonify({
+                'status':'FAILED',
+                'msg':'No Data for Updating'
+            })
+        
+        #Update Category
+        try:
+            category.name = new_name
+            db.session.commit()
+            return jsonify({
+                'status':'OK',
+                'msg':'Category Updated'
+            })
+        except IntegrityError:
+            db.session.rollback()
+            return jsonify({
+                'status':'FAILED',
+                'msg':'Failed to Update Category Name'
+            })
+        
+    else: return jsonify({
+        'status':'FAILED',
+        'msg':'No Category Found'
+    })
