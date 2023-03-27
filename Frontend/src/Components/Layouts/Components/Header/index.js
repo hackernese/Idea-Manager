a180f3e91c9d35d167bc1f08355c32e2039964e4
Hackernse <zenixcybercube2k2@gmail.com>
2023-03-27 22:20:50 +0700

    Hover effect on the user setting icon

--- Frontend/src/Components/Layouts/Components/Header/index.js
+++ Frontend/src/Components/Layouts/Components/Header/index.js
@@ -1,47 +1,47 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
-import logo from '../../../../Images/logo.png'
+import logo from '../../../../Images/logo.png';
+import user_setting from '../../../../Images/user.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown, faListUl } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 function Header() {
-
-    const [toggleState, setToggleState] = useState(true)
+    const [toggleState, setToggleState] = useState(true);
 
     return (
-        <div className={cx("wrapper",'d-flex','f-wrap')}>
+        <div className={cx('wrapper', 'd-flex', 'f-wrap')}>
             <div className={cx('col-2')}>
-                <div className={cx("logo","t-right")}>
+                <div className={cx('logo', 't-right')}>
                     <img src={logo}></img>
                 </div>
             </div>
             <div className={cx('responsive-btn')}>
-                <button
-                    onClick={()=>setToggleState(!toggleState)}
-                ><FontAwesomeIcon icon={faListUl}/></button>
+                <button onClick={() => setToggleState(!toggleState)}>
+                    <FontAwesomeIcon icon={faListUl} />
+                </button>
             </div>
-                    <div className={cx('col-9')}>
-                        <div className={cx("tab")}>
-                            <ul className={cx('d-flex','j-center')}>
-                                <li>Submitsion</li>
-                                <li>Category    </li>
-                                <li>Department</li>
-                                <li>Role</li>
-                                <li>User</li>
-                                <li>Staff Submission</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className={cx('col-1')}>
-                        <div className={cx("user-logo", "d-flex", "j-right","")}>
-                            <img src={logo}></img>
-                            <span>
-                                <FontAwesomeIcon icon={faCaretDown}/>
-                            </span>
-                        </div>
-                    </div>
+            <div className={cx('col-9')}>
+                <div className={cx('tab')}>
+                    <ul className={cx('d-flex', 'j-center')}>
+                        <li>Submitsion</li>
+                        <li>Category </li>
+                        <li>Department</li>
+                        <li>Role</li>
+                        <li>User</li>
+                        <li>Staff Submission</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={cx('col-1')}>
+                <div className={cx('user-logo', 'd-flex', 'j-right', '')}>
+                    <img src={user_setting}></img>
+                    <span>
+                        <FontAwesomeIcon icon={faCaretDown} />
+                    </span>
+                </div>
+            </div>
         </div>
     );
 }
