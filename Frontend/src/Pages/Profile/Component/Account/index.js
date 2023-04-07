d23db5081e8f454436555105ec1f605cee08b255
ArthurSeventh <hao.vv2001@gmail.com>
2023-04-07 20:37:30 +0700

    Updated interface for Account and Profile

--- Frontend/src/Pages/Profile/Component/Account/index.js
+++ Frontend/src/Pages/Profile/Component/Account/index.js
@@ -1,25 +1,79 @@
 import classNames from 'classnames/bind';
 import styles from './password.module.scss';
-import { useContext, useLayoutEffect } from 'react';
+import { createRef, useContext, useLayoutEffect, useState } from 'react';
 import { settingContext } from '../..';
+import CustomInput from '../../../../Components/CustomInput';
+import { useTranslation } from 'react-i18next';
+import { error, success, info } from '../../../../lib/toast';
+
 const cx = classNames.bind(styles);
 
 function Password() {
     const context = useContext(settingContext);
-    useLayoutEffect(() => context.settext('Account'), []);
+    const { t } = useTranslation();
+
+    const [loader1, setloader1] = useState(false);
+    const [loader2, setloader2] = useState(false);
+
+    const newpassref = createRef();
+    const cfpassref = createRef();
+    const currentpasswd = createRef();
+
+    useLayoutEffect(() => context.settext(t('setting.account.title')), []);
 
     return (
         <>
-            <div className={cx('container', 'd-flex', 'f-wrap')}>
-                <div className={cx('col-6')}>
-                    <div className={cx('content')}>
-                        <h1>Change Password</h1>
-                        <input type="text" placeholder="Current Password" />
-                        <input type="text" placeholder="New Password" />
-                        <input type="text" placeholder="Confirm New Password" />
-                        <br></br>
-                        <button>Update Password</button>
-                    </div>
+            <div className={cx('container')}>
+                <div>
+                    <h1>{t('setting.account.email_t')}</h1>
+                    <p>{t('setting.account.email_text')}</p>
+                    <CustomInput type="text" animation={false} placeholder="email@domain.ext"></CustomInput>
+                    {loader1 && <div className={cx('loader')}></div>}
+                    {!loader1 && (
+                        <button
+                            onClick={() => {
+                                info(
+                                    'The system has sent a confirmation message to your email, please confirm that this email is your in order to change it.',
+                                    10000,
+                                );
+
+                                // setloader1(true);
+                            }}
+                        >
+                            {t('setting.account.update')}
+                        </button>
+                    )}
+                </div>
+                <div>
+                    <h1>{t('setting.account.pass_t')}</h1>
+                    <p>{t('setting.account.pass_text')}</p>
+                    <CustomInput
+                        custom_ref={newpassref}
+                        animation={false}
+                        placeholder={t('setting.account.newpass')}
+                    ></CustomInput>
+                    <CustomInput
+                        custom_ref={cfpassref}
+                        animation={false}
+                        placeholder={t('setting.account.cpass')}
+                    ></CustomInput>
+                    <CustomInput
+                        custom_ref={currentpasswd}
+                        animation={false}
+                        placeholder={t('setting.account.currentpass')}
+                    ></CustomInput>
+
+                    {loader2 && <div className={cx('loader')}></div>}
+                    {!loader2 && (
+                        <button
+                            onClick={() => {
+                                success('Hello world');
+                                // setloader2(true);
+                            }}
+                        >
+                            {t('setting.account.update')}
+                        </button>
+                    )}
                 </div>
             </div>
         </>
--- Frontend/src/Pages/Profile/Component/ChangeProfile/index.js
+++ Frontend/src/Pages/Profile/Component/ChangeProfile/index.js
@@ -2,25 +2,55 @@ import classNames from 'classnames/bind';
 import styles from './changeProfile.module.scss';
 import { useContext, useLayoutEffect } from 'react';
 import { settingContext } from '../..';
+import CustomInput from '../../../../Components/CustomInput';
+import { useTranslation } from 'react-i18next';
+// import { error, success, info } from '../../../../lib/toast';
 
 const cx = classNames.bind(styles);
 
 function ChangeProfile() {
+    const { t } = useTranslation();
     const context = useContext(settingContext);
-    useLayoutEffect(() => context.settext('Profile'), []);
+    useLayoutEffect(() => context.settext(t('setting.profile.title')), []);
+
+    // username = db.Column(db.String(80), unique=True, nullable=False)
+
+    // # Optional columns / fields...
+    // birthday = db.Column(db.DateTime, nullable=True)
+    // gender = db.Column(db.String(50), nullable=False, default="unknown")
 
     return (
         <>
-            <div className={cx('container', 'd-flex', 'f-wrap')}>
-                <div className={cx('col-6')}>
-                    <div className={cx('content')}>
-                        <h1>Manage ChangeProfile</h1>
-                        <input type="text" placeholder="Username" />
-                        <input type="text" placeholder="Phone number" />
-                        <br></br>
-                        <button>Save</button>
-                    </div>
+            <div className={cx('container')}>
+                <div>
+                    <h1>{t('setting.profile.phone')}</h1>
+                    <p>{t('setting.profile.phonetext')}</p>
+                    <CustomInput
+                        type="text"
+                        animation={false}
+                        placeholder={t('setting.profile.phoneholder')}
+                    ></CustomInput>
                 </div>
+
+                <div>
+                    <h1>{t('setting.profile.address')}</h1>
+                    <p>{t('setting.profile.addresstext')}</p>
+                    <CustomInput
+                        type="text"
+                        animation={false}
+                        placeholder={t('setting.profile.addressholder')}
+                    ></CustomInput>
+                </div>
+
+                <div>
+                    <h1>{t('setting.profile.gender')}</h1>
+                </div>
+
+                <div>
+                    <h1>{t('setting.profile.birthday')}</h1>
+                </div>
+
+                <button>{t('setting.profile.update')}</button>
             </div>
         </>
     );
