78f4bcca09796f44057c1fe016626218be95b32c
Hackernse <zenixcybercube2k2@gmail.com>
2023-04-09 23:32:06 +0700

    New changes : 1. Changed the interface for Profile sidebar 2. added style.module.scss for HeaderOnly 3. Changed responsive design for main Profile

--- Frontend/src/Components/Layouts/HeaderOnly/index.js
+++ Frontend/src/Components/Layouts/HeaderOnly/index.js
@@ -1,11 +1,15 @@
 import Header from '../Components/Header';
+import classNames from 'classnames/bind';
+import style from './style.module.scss';
+
+const cx = classNames.bind(style);
 
 function DefaultLayout({ children }) {
     return (
         <div className={'wrapper'}>
             <Header />
             <div className="Container">
-                <div className="Content">{children}</div>
+                <div className={cx('mobile-content')}>{children}</div>
             </div>
         </div>
     );
