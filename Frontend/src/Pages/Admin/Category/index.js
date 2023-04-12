f240386f7f17ecabfd1d1c4b1b02a57ea0f4d74e
CaNeoooo <93911422+CaNeoooo@users.noreply.github.com>
2023-04-12 07:45:18 +0700

    1.Made a new pop up for adding a new category 2. Finished the layout for Category page 3. Made some changes to the table_style in globalMixins

--- Frontend/src/Pages/Admin/Category/index.js
+++ Frontend/src/Pages/Admin/Category/index.js
@@ -1,15 +1,42 @@
 import styles from './style.module.scss';
-import LoadingButton from '../../../Components/LoadingButton';
 import { useOutlet } from 'react-router-dom';
 import classNames from 'classnames';
 import axios from 'axios';
 import { createRef, useState } from 'react';
+import AnimatedOutlet from '../../../Components/AnimatedOutlet';
+import Popup from './popup';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash,faPenToSquare} from '@fortawesome/free-solid-svg-icons';
+
 
 function Category() {
     const outlet = useOutlet();
-    const refinput = createRef();
     const [cat, setcat] = useState([]);
+    const [showPopup, setShowPopup] = useState(false);
+    const refinput = createRef();
 
+    const handleButtonClick = () => {
+        setShowPopup(true);
+      };
+    
+    const handleClosePopup = () => {
+    setShowPopup(false);
+    };
+    const confbutton = () =>{
+        const temp = refinput.current.value;
+
+        axios.post('category.add', {
+            name:temp,
+        })
+        .then((resp)=>{console.log(resp.data);
+        });
+        
+
+    }
+
+
+
+    
     if (outlet) {
         return outlet;
     }
@@ -17,71 +44,54 @@ function Category() {
     console.log(cat);
 
     return (
-        <div className={styles.base}>
-            <div className={styles.header}>
-                <p>List of category</p>
-            </div>
-            <button
+        <AnimatedOutlet>
+            <div className={styles.base}>
+                <div className={styles.header}>
+                    <p>List of category</p>
+                </div>
+                <button
                 onClick={() => {
                     axios.post('category/list').then((resp) => {
-                        console.log(resp.data);
                         setcat(resp.data.msg);
                     });
                 }}
             >
                 CLick me to pull
             </button>
+                <button onClick= {handleButtonClick}>
+                    Create a new category
+                </button>
+                {showPopup && (<Popup
+          handleClose={handleClosePopup} refinput={refinput} confbutton={confbutton}/>)}
+                <div className={styles.main}>
+                    <div>
+                        <div>
+                            <div>
+                            <label>Name</label>
+                            <label>Date created</label>
+                            <label>Actions</label>
+                            </div>
+                        </div>
+                        <div>
+                            {cat.map((e,i) => {
+                                return(
+                                    <div key={i}>
+                                        <label>{e.name}</label>
+                                        <label>{e.created_on}</label>
+                                        <div className={styles.smallbuttons}>
+                                        <FontAwesomeIcon title="Edit Category" icon={faPenToSquare} />
+                                        <FontAwesomeIcon title="Delete Category" icon={faTrash} />
+                                        </div>
+                                    </div>
 
-            <button
-                onClick={() => {
-                    const temp = refinput.current.value;
+                                )
+                            })}
 
-                    axios
-                        .post('category/add', {
-                            name: temp,
-                        })
-                        .then((resp) => {
-                            console.log(resp.data);
-                        });
-                }}
-            >
-                Create a new category
-            </button>
-            <section>
-                <div>
-                    <label>Nameoisdfjdf</label>
-                    <label>Actions</label>
-                    <input ref={refinput} placeholder="Yopur category"></input>
-                </div>
-            </section>
-
-            {cat.map((e, index) => {
-                return (
-                    <div key={index}>
-                        <label>Created on : {e.created_on}</label>
-                        <label>Name : {e.name}</label>
-                        <label>ID : {e.id}</label>
-                        <button
-                            onClick={async () => {
-                                const v = refinput.current.value;
-
-                                await axios.post(`category/update/${e.id}`, {
-                                    name: v,
-                                });
-
-                                const resp = await axios.post('category/list');
-
-                                setcat(resp.data.msg);
-
-                                console.log('hello world');
-                            }}
-                        >
-                            Update
-                        </button>
+                        </div>
                     </div>
-                );
-            })}
-        </div>
+                </div>
+            </div>
+        </AnimatedOutlet>
     );
 }
 
