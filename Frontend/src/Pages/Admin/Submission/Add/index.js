48ed8c23cf032fbeeac67df78eea94405a37a287
CaNeoooo <93911422+CaNeoooo@users.noreply.github.com>
2023-04-18 02:05:29 +0700

    fix error

--- Frontend/src/Pages/Admin/Submission/Add/index.js
+++ Frontend/src/Pages/Admin/Submission/Add/index.js
@@ -32,9 +32,9 @@ function AddNewSubmission() {
                 <label>Name</label>
                 <CustomInput type="text" custom_ref={nameRef} placeholder="Submission name"></CustomInput>
                 <label>Deadline 1</label>
-                <DatePicker onChange={handleDeadline1}></DatePicker>
+                <DatePicker onChange={handleDeadline1} default_day={deadline1}></DatePicker>
                 <label>Deadline 2</label>
-                <DatePicker onChange={handleDeadline2} value></DatePicker>
+                <DatePicker onChange={handleDeadline2} default_day={deadline2}></DatePicker>
                 <div className={styles.wrapbutton}>
                     <button
                         onClick={() => {
@@ -45,6 +45,7 @@ function AddNewSubmission() {
                                     deadline2: deadline2,
                                 })
                                 .then((resp) => {
+                                    console.log(resp.data);
                                     if ((resp.data.status = 'FAIL')) {
                                         error('Name is already in the database');
                                     } else {
