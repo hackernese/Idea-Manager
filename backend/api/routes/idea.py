ca1287d4f613470f3422e89f42a1311c405b5ddc
hackernese <zenixcybercube2k2@gmail.com>
2023-04-18 13:34:24 +0700

    fixed issue in sending emails to coordinators

--- backend/api/routes/idea.py
+++ backend/api/routes/idea.py
@@ -1,7 +1,7 @@
 import os
 from app import app
 from . import login_required, send_mail
-from db import Submission, db, Idea, User, Reaction, Comments, Views, Role
+from db import Submission, db, Idea, User, Reaction, Comments, Views, Role, UserRoles
 from flask import jsonify, request, send_file
 from werkzeug.utils import secure_filename
 from sqlalchemy.exc import IntegrityError
@@ -106,15 +106,20 @@ def add_idea(submission_id):
             db.session.commit()
 
             # Auto send mail to Coordinator when having new idea
-            role = Role.query.filter(Role.name == "coordinator").first()
-            cor_users = User.query.filter(
-                User.userrole_ref.roleid == role.id).all()
+            role_id = Role.query.filter(Role.name == "coordinator").first().id
+
+            query_users = db.session.query(UserRoles).filter(
+                UserRoles.roleid == role_id)
+
             email = []
-            for cor_user in cor_users:
+            for cor_user in query_users:
                 email.append(cor_user.email)
+
+            print(email)
             # do func send_mail
-            send_mail("New Idea Was Uploaded!",
-                      f"{user.name} Has Created New Idea!", email)
+            if email:
+                send_mail("New Idea Was Uploaded!",
+                          f"{user.username} Has Created New Idea!", email)
 
             return jsonify({
                 'status': 'OK',
